56254202d3aaa8c77f36e017054aa822618f9020
Genevieve Conty <contygm@gmail.com>
2019-05-22 12:26:44 -0700

    dc3544: fixed lint issues

--- dynamic/react/api-app/containers/endpointContainer.js
+++ dynamic/react/api-app/containers/endpointContainer.js
@@ -45,6 +45,7 @@ const mapDispatchToProps = (dispatch) => {
                 } else {
                     const url = (endpoint.pathParams ? replaceStringPlaceholders(endpoint.path, reduceParamsToKeyValuePair(endpoint.pathParams)) : endpoint.path) + (endpoint.qsPath || '');
                     const postBody = endpoint.postBody || null;
+
                     apiRequest = submitApiRequest.bind(null, url, endpoint.action, postBody, userProfile);
                 }
 
