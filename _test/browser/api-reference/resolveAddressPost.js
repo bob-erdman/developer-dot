4c676019ad25c1eedb88d0cb2b68e2aca4fbbbab
Genevieve Conty <contygm@gmail.com>
2019-06-11 16:15:19 -0700

    dc3586: test for resolveAddress console fails as expected, testing behavior on reset, backspace/clear, and input

--- _test/browser/api-reference/resolveAddressPost.js
+++ _test/browser/api-reference/resolveAddressPost.js
@@ -1,7 +1,7 @@
 const deepEqual = require('../helpers/deepEqual');
 
 module.exports = {
-    '@tags': ['SKIP'],
+    // '@tags': ['SKIP'],
 
     'before': function(browser) {
         browser.maximizeWindow();
@@ -11,20 +11,56 @@ module.exports = {
         browser.end();
     },
 
-    'API Reference: AvaTax: REST v2 - API Console for ResolveAddressPost': function(browser) {
-        const expectedResponse = {address: {textCase: 'Upper', line1: '2000 Main Street', city: 'Irvine', region: 'CA', country: 'US', postalCode: '92614'}};
+    // 'API Reference: AvaTax: REST v2 - API Console for ResolveAddressPost': function(browser) {
+    //     const expectedResponse = {address: {textCase: 'Upper', line1: '2000 Main Street', city: 'Irvine', region: 'CA', country: 'US', postalCode: '92614'}};
 
+    //     browser
+    //         .initialize(browser.globals.baseURL + '/api-reference/avatax/rest/v2/methods/Addresses/ResolveAddressPost/');
+
+    //     browser.page.endpointSummary()
+    //         .navigateTo('#ResolveAddressPost-console')
+    //         .navigateTo('#ResolveAddressPost-console-body .fill-sample-data')
+    //         .click('#ResolveAddressPost-console-body .submit')
+
+    //         .getConsoleText('ResolveAddressPost', 'responseConsole', function(res) {
+    //             browser.assert.ok(deepEqual(res.address, expectedResponse.address),
+    //                 "response for 'try it now' matches expected response");
+    //         });
+    // },
+
+    'API Reference: AvaTax: REST v2 - API Console for ResolveAddress': function(browser) {
         browser
-            .initialize(browser.globals.baseURL + '/api-reference/avatax/rest/v2/methods/Addresses/ResolveAddressPost/');
-
-        browser.page.endpointSummary()
-            .navigateTo('#ResolveAddressPost-console')
-            .navigateTo('#ResolveAddressPost-console-body .fill-sample-data')
-            .click('#ResolveAddressPost-console-body .submit')
-
-            .getConsoleText('ResolveAddressPost', 'responseConsole', function(res) {
-                browser.assert.ok(deepEqual(res.address, expectedResponse.address),
-                    "response for 'try it now' matches expected response");
+            .initialize(browser.globals.baseURL + '/api-reference/avatax/rest/v2/methods/Addresses/ResolveAddress/')
+            .navigateTo('#ResolveAddress-console')
+            .navigateTo('#ResolveAddress-console-body')
+            .setValue('input', 'PO Box 1')
+            .getText(".code-snippet-plaintext", function(result) { 
+                browser.assert.ok(deepEqual(result.value, 'https://sandbox-rest.avatax.com/api/v2/addresses/resolve?line1=PO Box 1'), 'API Endpoint populates properly');
             });
+            // .getText("#ResolveAddress-console-body .console-req-container .code-snippet", function(result) { 
+            //     browser.assert.ok(deepEqual(result.value, "curl -X GET -H 'Accept: application/json' -H 'Authorization: Basic aHR0cHdhdGNoOmY=' https://sandbox-rest.avatax.com/api/v2/addresses/resolve?line1=PO Box 1"), 'cURL populates properly');
+            // })
+        // browser
+            // .click('#ResolveAddress-console-body span[type=reset]')
+            // .getText(".code-snippet-plaintext", function(result) { 
+            //     console.log("___RESULT____", result)
+            //     browser.assert.ok(deepEqual(result.value, 'https://sandbox-rest.avatax.com/api/v2/addresses/resolve'), 'API Endpoint resets properly');
+            // })
+            // .getText("#ResolveAddress-console-body .console-req-container .code-snippet", function(result) { 
+            //     browser.assert.ok(deepEqual(result.value, "curl -X GET -H 'Accept: application/json' -H 'Authorization: Basic aHR0cHdhdGNoOmY=' https://sandbox-rest.avatax.com/api/v2/addresses/resolve"), 'cURL resets properly');
+            // })
+            //.setValue('input', 'PO Box 1')
+        for (let index = 0; index < 'PO Box 1'.length; index++) {
+            console.log(index)
+            browser.keys(browser.Keys.BACK_SPACE);
+        }
+            
+        browser
+            .getText(".code-snippet-plaintext", function(result) { 
+                browser.assert.ok(deepEqual(result.value, 'https://sandbox-rest.avatax.com/api/v2/addresses/resolve'), 'API Endpoint clears when input is cleared');
+            })
+            .getText("#ResolveAddress-console-body .console-req-container .code-snippet", function(result) { 
+                browser.assert.ok(deepEqual(result.value, "curl -X GET -H 'Accept: application/json' -H 'Authorization: Basic aHR0cHdhdGNoOmY=' https://sandbox-rest.avatax.com/api/v2/addresses/resolve"), 'cURL clears when input is cleared');
+            })
     }
 };
--- _test/browser/api-reference/trustfile.js
+++ _test/browser/api-reference/trustfile.js
@@ -1,35 +1,35 @@
-const NUMAPIS = 3;
-let expectedNumberOfApiEndpoints;
+// const NUMAPIS = 3;
+// let expectedNumberOfApiEndpoints;
 
-module.exports = {
-    'before': function(browser) {
-        browser.maximizeWindow();
-    },
+// module.exports = {
+//     'before': function(browser) {
+//         browser.maximizeWindow();
+//     },
 
-    'after': function(browser) {
-        browser.end();
-    },
+//     'after': function(browser) {
+//         browser.end();
+//     },
 
-    'API Reference: TrustFile: Core (verify number of endpoints)': function(browser) {
-        expectedNumberOfApiEndpoints = 4;
+//     'API Reference: TrustFile: Core (verify number of endpoints)': function(browser) {
+//         expectedNumberOfApiEndpoints = 4;
 
-        browser
-            .initialize(browser.globals.baseURL + '/api-reference/trustfile/api/methods/GetItems/')
-            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
-    },
-    'API Reference: TrustFile: Provisioning (verify number of endpoints)': function(browser) {
-        expectedNumberOfApiEndpoints = 3;
+//         browser
+//             .initialize(browser.globals.baseURL + '/api-reference/trustfile/api/methods/GetItems/')
+//             .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
+//     },
+//     'API Reference: TrustFile: Provisioning (verify number of endpoints)': function(browser) {
+//         expectedNumberOfApiEndpoints = 3;
 
-        browser
-            .initialize(browser.globals.baseURL + '/api-reference/trustfile/app/methods/PostCompanySignup/')
-            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
-    },
-    'API Reference: TrustFile: Connector (verify number of endpoints)': function(browser) {
-        expectedNumberOfApiEndpoints = 14;
+//         browser
+//             .initialize(browser.globals.baseURL + '/api-reference/trustfile/app/methods/PostCompanySignup/')
+//             .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
+//     },
+//     'API Reference: TrustFile: Connector (verify number of endpoints)': function(browser) {
+//         expectedNumberOfApiEndpoints = 14;
 
-        browser
-            .initialize(browser.globals.baseURL + '/api-reference/trustfile/connector-microservice/methods/GetConnections/')
-            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
-    }
+//         browser
+//             .initialize(browser.globals.baseURL + '/api-reference/trustfile/connector-microservice/methods/GetConnections/')
+//             .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
+//     }
 
-};
+// };
